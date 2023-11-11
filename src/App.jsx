f2013d2057b52568a297eb145b89ef2ec7e7e9f9
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-11-11 12:27:32 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="main">
+            <h1>
+              Gera Samba
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
